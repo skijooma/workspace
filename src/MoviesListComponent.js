33f2b18cea81ec3e7a648a2169fbec8cf4b7b13e
skijooma <skijooma@andrew.cmu.edu>
2020-07-01 13:47:50 +0000

    Created a movies list component

---
+++ src/MoviesListComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class MoviesListComponent extends Component {
+
+  	render(){
+    
+    	return (
+        	
+          <ol>
+          {
+          	<h2></h2>
+          }
+          </ol>
+        );
+    }
+}
+
+export default MoviesListComponent;
