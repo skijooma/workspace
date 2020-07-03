101f906c74e5b741cf81840d6441e39874c1ddab
skijooma <skijooma@andrew.cmu.edu>
2020-07-03 08:21:53 +0000

    Converted project Class Components to Stateless Functional Components

--- src/MovieCardsList.js
+++ src/MovieCardsList.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MovieCard from './MovieCard';
 
-class MovieCardsList extends Component {
-  render() {
-    /*
-  Destructuring via ES6. We're getting the profiles, users, and movies properties
-  off of the pros passed into this presentational component. If you need a refresher on this syntax, check
-  out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
-  */
+function MovieCardsList (props) {
+	
+	/*
+    Destructuring via ES6. We're getting the profiles, users, and movies properties
+    off of the pros passed into this presentational component. If you need a refresher on this syntax, check
+    out this course: https://www.udacity.com/course/es6-javascript-improved--ud356
+    */
 
-    const { profiles, users, movies } = this.props;
+    const { profiles, users, movies } = props;
     const usersByMovie = {};
 
     profiles.forEach(profile => {
@@ -21,7 +21,7 @@ class MovieCardsList extends Component {
         usersByMovie[movieID] = [profile.userID];
       }
     });
-
+    
     const movieCards = Object.keys(movies).map(id => (
       <MovieCard
         key={id}
@@ -31,11 +31,12 @@ class MovieCardsList extends Component {
       />
     ));
 
-    /*
+	/*
     Return JSX
     */
-    return <ul>{movieCards}</ul>;
-  }
+  	return (
+    	<ul>{movieCards}</ul>
+    );
 }
 
 export default MovieCardsList;
